7bbdb18ea58457dacea2807506a11c10ba340619
cornel <ghita_cornel@yahoo.com>
2018-07-03 15:13:15 +0300

    better tests

--- src/main/java/beans/timers/Timer.java
+++ src/main/java/beans/timers/Timer.java
@@ -1,6 +1,6 @@
 package beans.timers;
 
-import org.springframework.scheduling.annotation.Scheduled;
+//import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import java.util.Date;
@@ -8,7 +8,7 @@ import java.util.Date;
 @Component
 public class Timer {
 
-    @Scheduled(fixedRate = 100)
+//    @Scheduled(fixedRate = 100)
     public void reportCurrentTime() {
         System.out.println(this + " stupid timer was triggered at " + new Date());
     }
