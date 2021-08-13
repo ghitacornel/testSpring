2ce2af766cd6f3037f47ab23ef7f8ca1998445b9
cornel <ghita.cornel@gmail.com>
2021-08-13 11:48:57 +0300

    better

--- src/main/java/beans/factories/misc/FactoryMisc.java
+++ src/main/java/beans/factories/misc/FactoryMisc.java
@@ -7,13 +7,13 @@ import org.springframework.context.annotation.Configuration;
 public class FactoryMisc {
 
     @Bean
-    Product1 product1() {
+    public Product1 product1() {
         Product1 product1 = new Product1();// observe product1 only once creation and the reused
         return product1;
     }
 
     @Bean
-    Product2 product2() {
+    public Product2 product2() {
         Product1 product1 = product1();// observe "product1" not invoked, reused same product
         return new Product2(product1);
     }
