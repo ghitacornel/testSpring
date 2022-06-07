ba2ebb86239f937ca65d604aed341cef6991c56e
cornel <ghita.cornel@gmail.com>
2022-06-07 16:24:09 +0300

    better

--- src/main/java/beans/events/EventPublisher.java
+++ src/main/java/beans/events/EventPublisher.java
@@ -4,16 +4,27 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
 
+/**
+ * My simple producer of events
+ */
 @Component
 public class EventPublisher {
 
+    /**
+     * Spring event firing mechanism
+     */
     @Autowired
     private ApplicationEventPublisher containerPublisher;
 
     public void publishEvent(String message) {
         System.out.println("Publishing custom event with message : " + message);
+
+        // I build my event
         CustomSpringEvent customSpringEvent = new CustomSpringEvent(this, message);
+
+        // I fire my event
         containerPublisher.publishEvent(customSpringEvent);
+
     }
 
 }
