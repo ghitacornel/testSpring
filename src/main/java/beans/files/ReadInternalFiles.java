3b12ce8835f15d1f2a8132028321d52ce77a9e4e
cornel <ghita.cornel@gmail.com>
2022-10-26 20:34:39 +0300

    better

--- src/main/java/beans/files/ReadInternalFiles.java
+++ src/main/java/beans/files/ReadInternalFiles.java
@@ -2,18 +2,21 @@ package beans.files;
 
 import org.springframework.stereotype.Component;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
+import java.util.stream.Collectors;
 
 @Component
 public class ReadInternalFiles {
 
     public String readFileContent() {
         try {
-            Path path = Paths.get("src", "main", "resources", "templates", "internal_file.txt");
-            byte[] bytes = Files.readAllBytes(path);
-            return new String(bytes);
+            InputStream inputStream = ReadInternalFiles.class.getResourceAsStream("/templates/internal_file.txt");
+            return new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8))
+                    .lines()
+                    .collect(Collectors.joining("\n"));
         } catch (Exception e) {
             throw new RuntimeException(e);
         }
