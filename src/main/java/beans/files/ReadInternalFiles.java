9dcfab619f7647464953097b194962e79d1164c4
cornel <ghita.cornel@gmail.com>
2022-10-26 20:02:44 +0300

    better

--- src/main/java/beans/files/ReadInternalFiles.java
+++ src/main/java/beans/files/ReadInternalFiles.java
@@ -3,7 +3,6 @@ package beans.files;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
-import java.net.URL;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
@@ -13,7 +12,6 @@ public class ReadInternalFiles {
 
     public String readFileContent() {
         try {
-            URL resource = ReadInternalFiles.class.getResource("templates/my_template.txt");
             Path path = Paths.get("src", "main", "resources", "templates", "my_template.txt");
             File file = path.toFile();
             byte[] bytes = Files.readAllBytes(file.toPath());
