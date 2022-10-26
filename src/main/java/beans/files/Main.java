6bd6585db38f6b3339488ffcc2a2ad0602812cf0
cornel <ghita.cornel@gmail.com>
2022-10-26 20:04:31 +0300

    better

---
+++ src/main/java/beans/files/Main.java
@@ -0,0 +1,12 @@
+package beans.files;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Main {
+    public static void main(String[] args) {
+        ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("spring-beans.xml");
+        ReadInternalFiles bean = context.getBean(ReadInternalFiles.class);
+        String content = bean.readFileContent();
+        System.out.println(content);
+    }
+}
